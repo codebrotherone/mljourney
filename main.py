51c56d4bb79e068fab06c6cf5a906762bb4d669b
Jae Han <jae.han@ibm.com>
2018-07-28 11:21:15 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Based on : http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
+
+To better understand how neural networks work, I should create one from scratch. Initially this tutorial doesn't
+derive all functionality and takes some things for granted (data generation, as well as some of the `mathy` functions)
+
+In time I hope to update everything that is required.
+
+"""
+
