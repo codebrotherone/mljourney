312d6494efc06ee8bfd7b1a6a42fc15f068b5035
Jae Han <jae.han@ibm.com>
2018-07-29 17:13:51 -0500

    updated new reference for NN and changed names

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """"
 ________________________________________________________________________________________________________________________
 Based on : http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
+		   https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
 
 To better understand how neural networks work, I should create one from scratch. Initially this tutorial doesn't
 derive all functionality and takes some things for granted (data generation, as well as some of the `mathy` functions)
@@ -15,7 +16,7 @@ import numpy as np
 
 #
 
-class ANN(object):
+class NeuralNetwork(object):
 	"""The model used to generate the artificial neural network
 
 	It will create a neural network that can identify """
@@ -23,11 +24,14 @@ class ANN(object):
 		self.data = make_moons(200, noise=0.20)
 		self.X = self.data[0]
 		self.y = self.data[1]
-		self._single = 'single'
-		self.__dunder = 'dunder'
-		self.seed = 0
-		np.seed(self.seed)
-	
+		self.w1 = np.random.rand(self.X.shape[1], 4)
+		self.w2 = np.random.rand(4, 1)
+		self.output = np.zeros(self.y.shape)
+
+		# unsure of best place to put this.
+		# self.seed = 0
+		# np.seed(self.seed)
+
 	def __repr__(self):
 		""" For debugging """
 		return "ANN()"
