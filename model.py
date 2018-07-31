30b2f660fc927bff4a44f44fcd9b0cead32c8b64
Jae Han <jae.han@ibm.com>
2018-07-31 13:20:25 -0500

    added .idea to gitignore and updated model

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 ________________________________________________________________________________________________________________________
 Based on : http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
 		   https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
-
+		   http://dataaspirant.com/2017/03/07/difference-between-softmax-function-and-sigmoid-function/
 To better understand how neural networks work, I should create one from scratch. Initially this tutorial doesn't
 derive all functionality and takes some things for granted (data generation, as well as some of the `mathy` functions)
 
@@ -31,6 +31,18 @@ class NeuralNetwork(object):
 		# unsure of best place to put this.
 		# self.seed = 0
 		# np.seed(self.seed)
+	def calc_loss(self):
+		W1, b1, W2, b2 = self.model['W1'], self.model['b1'], self.model['W2'], self.model['b2']
+		# calculate the first layer input
+		z1 = np.dot(self.X, W1)
+		# activation of first input
+		a1 = np.tanh(z1)
+		# calculate second layer input
+		z2 = np.dot(a1, W2) + b2
+		# this is our yhat value (softmax(z2)
+		# also, calculate the loss by finding actual values
+		a2 = np.exp(z2)/float(np.sum(np.exp(z2), axis=1, ))
+		actual = -np.log
 
 	def __repr__(self):
 		""" For debugging """
